6001897ac071633351b8ea8dfc67a00a84331c81
BenSeddikMehdi <amigoinpt@gmail.com>
2019-11-29 23:50:21 +0100

    Printing the file main.c to stdout

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <fcntl.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char **argv) {
+    // open the file
+    int fd = open(argv[1], O_RDONLY);
+    // read its contents and print them to stdout
+    char c;
+    while (read(fd, &c, 1) > 0) {
+        fputc(c, stdout);
+    }
+    close(fd);
     return 0;
 }
